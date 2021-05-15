623a20eaec2f1e9e792cb9bb49f710390710f788
Connor Todd <connortodd0304@gmail.com>
2021-05-15 10:42:20 +0100

    dropdown menu implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = React.useState([]);
+  const [countrySelected, setCountrySelected] = React.useState("Afghanistan");
+
+  React.useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = () => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((response) => response.json())
+      .then((data) => setCountries(data));
+  };
+
+  const handleCountryChange = (event) => {
+    setCountrySelected(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* dropdown to select country */}
+      <select onChange={handleCountryChange}>
+        {countries.map((item) => (
+          <option value={item.name}>{item.name}</option>
+        ))}
+      </select>
+
+      {/* Display single country */}
+      {countries
+        .filter((country) => country.name === countrySelected)
+        .map((item) => (
+          <div>
+            <h1>{item.name}</h1>
+            <img src={item.flag} height="200" width="200" />
+            {item.languages.map((language) => (
+              <p>{language.nativeName}</p>
+            ))}
+            <p>{item.translations.ja}</p>
+            {item.currencies.map((currency) => (
+              <p>{currency.name}</p>
+            ))}
+          </div>
+        ))}
     </div>
   );
 }
